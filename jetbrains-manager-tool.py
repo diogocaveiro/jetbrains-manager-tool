da63f4fc0f6849cc4fbc48545175ca60dd62add1
diogocaveiro <diogocaveiro.git@mailbox.org>
2023-10-14 22:44:21 +0100

    Improved and fixed installation

--- jetbrains-manager-tool.py
+++ jetbrains-manager-tool.py
@@ -5,6 +5,7 @@
         - Update all
         - Update selected apps
         - Change default path
+        - Update mimeapps.list
 """
 
 import requests
@@ -12,13 +13,13 @@ import argparse
 import os
 import subprocess
 import sys
-import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as elementTree
 
 JETBRAINS_XML_URL = "https://www.jetbrains.com/updates/updates.xml"
 JETBRAINS_INSTALL_PATH = "/opt/jetbrains/"
 
 APP_LIST = {
-    "pycharm": {
+    "pycharm-professional": {
         "flag": "-p",
         "long_flag": "--pycharm",
         "help": "PyCharm Professional",
@@ -26,12 +27,173 @@ APP_LIST = {
         "name": "PyCharm",
         "channel_name": "PyCharm RELEASE",
         "download-link": "https://download.jetbrains.com/python/pycharm-professional-<VERSION>.tar.gz",
+        "comment": "The full stack Python IDE",
+        "executable": "pycharm",
     },
     "clion": {
         "flag": "-c",
         "long_flag": "--clion",
         "help": "CLion",
         "folder": "clion",
+        "name": "CLion",
+        "channel_name": "CLion RELEASE",
+        "download-link": "https://download.jetbrains.com/cpp/CLion-<VERSION>.tar.gz",
+        "comment": "A cross-platform IDE for C and C++",
+        "executable": "clion",
+    },
+    "datagrip": {
+        "flag": "-d",
+        "long_flag": "--datagrip",
+        "help": "DataGrip",
+        "folder": "datagrip",
+        "name": "DataGrip",
+        "channel_name": "DataGrip RELEASE",
+        "download-link": "https://download.jetbrains.com/datagrip/datagrip-<VERSION>.tar.gz",
+        "comment": "Your Swiss Army Knife for Databases and SQL",
+        "executable": "datagrip",
+    },
+    "goland": {
+        "flag": "-g",
+        "long_flag": "--goland",
+        "help": "GoLand",
+        "folder": "goland",
+        "name": "GoLand",
+        "channel_name": "GoLand RELEASE",
+        "download-link": "https://download.jetbrains.com/go/goland-<VERSION>.tar.gz",
+        "comment": "A Clever IDE to Go",
+        "executable": "goland",
+    },
+    "intellij-community": {
+        "flag": "-n",
+        "long_flag": "--intellij-community",
+        "help": "IntelliJ IDEA Community",
+        "folder": "ideaIC",
+        "name": "IntelliJ IDEA",
+        "channel_name": "IntelliJ IDEA RELEASE",
+        "download-link": "https://download.jetbrains.com/idea/ideaIC-<VERSION>.tar.gz",
+        "comment": "Capable and Ergonomic IDE for JVM",
+        "executable": "idea",
+    },
+    "intellij-ultimate": {
+        "flag": "-m",
+        "long_flag": "--intellij-ultimate",
+        "help": "IntelliJ IDEA Ultimate",
+        "folder": "ideaIU",
+        "name": "IntelliJ IDEA",
+        "channel_name": "IntelliJ IDEA RELEASE",
+        "download-link": "https://download.jetbrains.com/idea/ideaIU-<VERSION>.tar.gz",
+        "comment": "Capable and Ergonomic IDE for JVM",
+        "executable": "idea",
+    },
+    "phpstorm": {
+        "flag": "-k",
+        "long_flag": "--phpstorm",
+        "help": "PhpStorm",
+        "folder": "phpstorm",
+        "name": "PhpStorm",
+        "channel_name": "PhpStorm RELEASE",
+        "download-link": "https://download.jetbrains.com/webide/PhpStorm-<VERSION>.tar.gz",
+        "comment": "The Lightning-Smart IDE for PHP Programming by JetBrains",
+        "executable": "phpstorm",
+    },
+    "pycharm-community": {
+        "flag": "-y",
+        "long_flag": "--pycharm-community",
+        "help": "PyCharm Community",
+        "folder": "pycharm-community",
+        "name": "PyCharm Community",
+        "channel_name": "PyCharm RELEASE",
+        "download-link": "https://download.jetbrains.com/python/pycharm-community-<VERSION>.tar.gz",
+        "comment": "The Python IDE for Professional Developers",
+        "executable": "pycharm",
+    },
+    "pycharm-edu": {
+        "flag": "-e",
+        "long_flag": "--pycharm-edu",
+        "help": "PyCharm Edu",
+        "folder": "pycharm-edu",
+        "name": "PyCharm Edu",
+        "channel_name": "PyCharm Edu RELEASE",
+        "download-link": "https://download.jetbrains.com/python/pycharm-edu-<VERSION>.tar.gz",
+        "comment": "The Python IDE for Professional Developers",
+        "executable": "pycharm",
+    },
+    "rider": {
+        "flag": "-r",
+        "long_flag": "--rider",
+        "help": "Rider",
+        "folder": "rider",
+        "name": "Rider",
+        "channel_name": "Rider RELEASE",
+        "download-link": "https://download.jetbrains.com/rider/JetBrains.Rider-<VERSION>.tar.gz",
+        "comment": "Cross-platform .NET IDE based on IntelliJ platform and ReSharper",
+        "executable": "rider",
+    },
+    "rubymine": {
+        "flag": "-b",
+        "long_flag": "--rubymine",
+        "help": "RubyMine",
+        "folder": "rubymine",
+        "name": "RubyMine",
+        "channel_name": "RubyMine RELEASE",
+        "download-link": "https://download.jetbrains.com/ruby/RubyMine-<VERSION>.tar.gz",
+        "comment": "The Most Intelligent Ruby and Rails IDE",
+        "executable": "rubymine",
+    },
+    "webstorm": {
+        "flag": "-w",
+        "long_flag": "--webstorm",
+        "help": "WebStorm",
+        "folder": "webstorm",
+        "name": "WebStorm",
+        "channel_name": "WebStorm RELEASE",
+        "download-link": "https://download.jetbrains.com/webstorm/WebStorm-<VERSION>.tar.gz",
+        "comment": "The Smartest JavaScript IDE",
+        "executable": "webstorm",
+    },
+    "appcode": {
+        "flag": "-a",
+        "long_flag": "--appcode",
+        "help": "AppCode",
+        "folder": "appcode",
+        "name": "AppCode",
+        "channel_name": "AppCode RELEASE",
+        "download-link": "https://download.jetbrains.com/objc/AppCode-<VERSION>.tar.gz",
+        "comment": "Smart IDE for iOS/macOS development",
+        "executable": "appcode",
+    },
+    "aquacode": {
+        "flag": "-q",
+        "long_flag": "--aquacode",
+        "help": "AquaCode",
+        "folder": "aquacode",
+        "name": "AquaCode",
+        "channel_name": "AquaCode RELEASE",
+        "download-link": "https://download.jetbrains.com/aquacode/AquaCode-<VERSION>.tar.gz",
+        "comment": "A powerful IDE for test automation",
+        "executable": "aquacode",
+    },
+    "dataspell": {
+        "flag": "-s",
+        "long_flag": "--dataspell",
+        "help": "DataSpell",
+        "folder": "dataspell",
+        "name": "DataSpell",
+        "channel_name": "DataSpell RELEASE",
+        "download-link": "https://download.jetbrains.com/dataspell/DataSpell-<VERSION>.tar.gz",
+        "comment": "The data science IDE",
+        "executable": "dataspell",
+    },
+    "rustrover": {
+        "flag": "-o",
+        "long_flag": "--rustrover",
+        "help": "RustRover",
+        "folder": "rustrover",
+        "name": "RustRover",
+        "channel_name": "RustRover RELEASE",
+        "download-link": "https://download.jetbrains.com/rustrover/RustRover-<VERSION>.tar.gz",
+        "comment": "The IDE for Rust",
+        "executable": "rustrover",
     },
 }
 
@@ -128,7 +290,7 @@ class JetbrainsManagerTool:
         Installs selected apps.
         TODO: Check if app is already installed.
         """
-        root = ET.fromstring(self.xml_file)
+        root = elementTree.fromstring(self.xml_file)
 
         for selected_app in self.selected_apps:
             # Define install path
@@ -192,11 +354,12 @@ class JetbrainsManagerTool:
                 with open(desktop_entry_path, 'w') as f:
                     f.write("[Desktop Entry]\n")
                     f.write("Name={}\n".format(APP_LIST[selected_app]['help']))
-                    f.write("Exec={}/bin/{}.sh\n".format(install_path, selected_app))
-                    f.write("Icon={}/bin/{}.png\n".format(install_path, selected_app))
+                    f.write("Exec={}/bin/{}.sh\n".format(install_path, APP_LIST[selected_app]['executable']))
+                    f.write("Icon={}/bin/{}.png\n".format(install_path, APP_LIST[selected_app]['executable']))
                     f.write("Terminal=false\n")
                     f.write("Type=Application\n")
                     f.write("Categories=Development;\n")
+                    f.write("Comment={}\n".format(APP_LIST[selected_app]['comment']))
 
                 print("Successfully created desktop entry at {}".format(desktop_entry_path))
             except Exception as e:
@@ -210,7 +373,7 @@ class JetbrainsManagerTool:
                     print("Symlink already exists. Deleting it.")
                     os.remove(symlink_path)
 
-                os.symlink(os.path.join(install_path, 'bin', selected_app + '.sh'), symlink_path)
+                os.symlink(os.path.join(install_path, 'bin', APP_LIST[selected_app]['executable'] + '.sh'), symlink_path)
 
                 print("Successfully created symlink at {}".format(symlink_path))
             except Exception as e:
@@ -218,9 +381,16 @@ class JetbrainsManagerTool:
 
             # Chmod +x on executable
             try:
-                executable_path = os.path.join(install_path, 'bin', selected_app + '.sh')
+                executable_path = os.path.join(install_path, 'bin', APP_LIST[selected_app]['executable'] + '.sh')
                 subprocess.call(['sudo', 'chmod', '+x', executable_path])
                 print("Successfully set executable permissions on {}".format(executable_path))
+            except Exception as e:
+                print(e)
+
+            # Remove downloaded file
+            try:
+                os.remove(download_path)
+                print("Successfully removed downloaded file at {}".format(download_path))
             except Exception as e:
                 print(e)
 
