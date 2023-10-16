c278fdd494e7a4e25e48dc02c45a027b7888d142
diogocaveiro.it@mailbox.org <diogocaveiro.git@mailbox.org>
2023-10-16 11:04:39 +0100

    Added fetch installed apps version

--- jetbrains-manager-tool.py
+++ jetbrains-manager-tool.py
@@ -6,6 +6,7 @@
         - Update selected apps
         - Change default path
         - Update mimeapps.list
+        - Check if download link exists before download
 """
 
 import requests
@@ -14,6 +15,7 @@ import os
 import subprocess
 import sys
 import xml.etree.ElementTree as elementTree
+import json
 
 JETBRAINS_XML_URL = "https://www.jetbrains.com/updates/updates.xml"
 ANDROID_STUDIO_XML_URL = "https://dl.google.com/android/studio/patches/updates.xml"
@@ -28,7 +30,7 @@ APP_LIST = {
         "name": "Android Studio",
         "channel_name": "Android Studio updates",
         "download-link": "https://redirector.gvt1.com/edgedl/android/studio/ide-zips/<VERSION>/android-studio-<VERSION>-linux.tar.gz",
-        "comment": "An IDE for Android app development.",
+        "comment": "An IDE for Android app development",
         "executable": "studio",
     },
     "pycharm-professional": {
@@ -192,7 +194,7 @@ APP_LIST = {
         "folder": "dataspell",
         "name": "DataSpell",
         "channel_name": "DataSpell RELEASE",
-        "download-link": "https://download.jetbrains.com/dataspell/DataSpell-<VERSION>.tar.gz",
+        "download-link": "https://download.jetbrains.com/python/dataspell-<VERSION>.tar.gz",
         "comment": "The data science IDE",
         "executable": "dataspell",
     },
@@ -266,21 +268,29 @@ class JetbrainsManagerTool:
 
         # Update mode
         if args.update:
-            pass
-            # TODO: Update
+            self.__install(update=True)
 
     def __check_installed_apps(self):
         """
-        Checks installed apps.
+        Checks installed applications and its versions.
         """
-        self.installed_apps = []
+        # Check installed applications
+        self.installed_apps = {}
         for key, value in APP_LIST.items():
-            if os.path.exists(os.path.join("/opt", value["folder"])):
-                self.installed_apps.append(key)
+            install_path = os.path.join(JETBRAINS_INSTALL_PATH, value["folder"])
+            print(install_path)
+            if os.path.exists(install_path):
+                print('exists')
+                with open(os.path.join(install_path, 'product-info.json'), 'r') as json_file:
+                    version = json.load(json_file)['version']
+                    print(f'{key}: {version}')
+                self.installed_apps[key] = version
+
+        # Print result
         if self.installed_apps:
             print(
                 "The following apps are already installed:\n"
-                + "\n".join(self.installed_apps)
+                + "\n".join(self.installed_apps.keys())
             )
         else:
             print("No app installed in the designated install folder.")
@@ -309,47 +319,89 @@ class JetbrainsManagerTool:
 
             self.xml_file = elementTree.tostring(jetbrains_root, encoding="utf-8")
 
-            print("Successfully fetched and combined JetBrains and Android Studio XML files.")
+            print(
+                "Successfully fetched and combined JetBrains and Android Studio XML files."
+            )
 
         except Exception as e:
             print(e)
 
-    def __install(self):
+    def __update(self):
+        """
+        Updates selected or all installed apps.
+        """
+        pass
+
+    def __install(self, update=False):
         """
         Installs selected apps.
         """
         root = elementTree.fromstring(self.xml_file)
 
+        # Check outdated applications
+        if update:
+            print('UPDATE CALLED')
+            outdated_apps = []
+            for installed_app, installed_version in self.installed_apps.items():
+                version_query = "./product[@name='{}']/channel[@name='{}']/build".format(
+                    APP_LIST[installed_app]["name"], APP_LIST[installed_app]["channel_name"]
+                )
+                version_elements = root.findall(version_query)
+
+                if version_elements:
+                    last_version = version_elements[0].get("version")
+
+                    if installed_app == "android-studio":
+                        last_version = last_version.split(" ")[2]
+                else:
+                    print(
+                        "Couldn't find data for {}".format(APP_LIST[installed_app]["name"])
+                    )
+
+                if installed_version != last_version:
+                    outdated_apps.append(installed_app)
+
+            print(outdated_apps)
+
+            return
+
         for selected_app in self.selected_apps:
             # Define install path
-            install_path = os.path.join(JETBRAINS_INSTALL_PATH, APP_LIST[selected_app]['folder'])
+            install_path = os.path.join(
+                JETBRAINS_INSTALL_PATH, APP_LIST[selected_app]["folder"]
+            )
 
             # Check if app is already installed
             if os.path.exists(install_path):
-                print(f"{APP_LIST[selected_app]['name']} is already installed. Skipping installation.")
+                print(
+                    f"{APP_LIST[selected_app]['name']} is already installed. Skipping installation."
+                )
                 continue
 
             # Check last version
             version_query = "./product[@name='{}']/channel[@name='{}']/build".format(
-                APP_LIST[selected_app]['name'],
-                APP_LIST[selected_app]['channel_name']
+                APP_LIST[selected_app]["name"], APP_LIST[selected_app]["channel_name"]
             )
             version_elements = root.findall(version_query)
 
             if version_elements:
-                last_version = version_elements[0].get('version')
+                last_version = version_elements[0].get("version")
 
                 if selected_app == "android-studio":
                     last_version = last_version.split(" ")[2]
             else:
-                print("Couldn't find data for {}".format(APP_LIST[selected_app]['name']))
+                print(
+                    "Couldn't find data for {}".format(APP_LIST[selected_app]["name"])
+                )
 
             # Download latest version
             if selected_app == "android-studio":
                 valid_link_found = False
                 for suffix in reversed(range(1, 30)):
-                    last_version_try = last_version + '.' + str(suffix)
-                    temp_link = APP_LIST[selected_app]["download-link"].replace("<VERSION>", last_version_try)
+                    last_version_try = last_version + "." + str(suffix)
+                    temp_link = APP_LIST[selected_app]["download-link"].replace(
+                        "<VERSION>", last_version_try
+                    )
                     final_status = self.check_redirect(temp_link)
 
                     if final_status == 200:
@@ -358,15 +410,21 @@ class JetbrainsManagerTool:
                         break
 
                 if not valid_link_found:
-                    print("Error. Could not find a valid download link for Android Studio. Aborting installation.")
+                    print(
+                        "Error. Could not find a valid download link for Android Studio. Aborting installation."
+                    )
                     continue
 
             else:
-                download_link = APP_LIST[selected_app]["download-link"].replace("<VERSION>", last_version)
+                download_link = APP_LIST[selected_app]["download-link"].replace(
+                    "<VERSION>", last_version
+                )
 
             print(f"Downloading {APP_LIST[selected_app]['name']} from {download_link}")
-            download_path = os.path.join(os.getcwd(),
-                                         '{}-{}.tar.gz'.format(APP_LIST[selected_app]['name'], last_version))
+            download_path = os.path.join(
+                os.getcwd(),
+                "{}-{}.tar.gz".format(APP_LIST[selected_app]["name"], last_version),
+            )
 
             if os.path.exists(download_path):
                 print("File already exists. Skipping download.")
@@ -375,10 +433,12 @@ class JetbrainsManagerTool:
                     req = requests.get(download_link)
                     assert req.status_code == 200
 
-                    with open(download_path, 'wb') as f:
+                    with open(download_path, "wb") as f:
                         f.write(req.content)
 
-                    print("Successfully downloaded app file to {}".format(download_path))
+                    print(
+                        "Successfully downloaded app file to {}".format(download_path)
+                    )
                 except Exception as e:
                     print(e)
 
@@ -387,42 +447,73 @@ class JetbrainsManagerTool:
             if not os.path.exists(install_path):
                 os.makedirs(install_path)
                 try:
-                    subprocess.call(['sudo', 'tar', '-xzf', download_path, '-C', install_path, '--strip-components=1'])
-                    print("Successfully extracted file to {}".format(install_path))
+                    result = subprocess.call(
+                        [
+                            "sudo",
+                            "tar",
+                            "-xzf",
+                            download_path,
+                            "-C",
+                            install_path,
+                            "--strip-components=1",
+                        ]
+                    )
+                    assert result != 0
                 except Exception as e:
                     print(e)
 
             # Create desktop entry
             try:
-                desktop_entry_path = os.path.join('/usr/share/applications', '{}.desktop'.format(selected_app))
+                desktop_entry_path = os.path.join(
+                    "/usr/share/applications", "{}.desktop".format(selected_app)
+                )
 
                 if os.path.exists(desktop_entry_path):
                     print("Desktop entry already exists. Deleting it.")
                     os.remove(desktop_entry_path)
 
-                with open(desktop_entry_path, 'w') as f:
+                with open(desktop_entry_path, "w") as f:
                     f.write("[Desktop Entry]\n")
-                    f.write("Name={}\n".format(APP_LIST[selected_app]['help']))
-                    f.write("Exec={}/bin/{}.sh\n".format(install_path, APP_LIST[selected_app]['executable']))
-                    f.write("Icon={}/bin/{}.png\n".format(install_path, APP_LIST[selected_app]['executable']))
+                    f.write("Name={}\n".format(APP_LIST[selected_app]["help"]))
+                    f.write(
+                        "Exec={}/bin/{}.sh\n".format(
+                            install_path, APP_LIST[selected_app]["executable"]
+                        )
+                    )
+                    f.write(
+                        "Icon={}/bin/{}.png\n".format(
+                            install_path, APP_LIST[selected_app]["executable"]
+                        )
+                    )
                     f.write("Terminal=false\n")
                     f.write("Type=Application\n")
                     f.write("Categories=Development;\n")
-                    f.write("Comment={}\n".format(APP_LIST[selected_app]['comment']))
+                    f.write("Comment={}\n".format(APP_LIST[selected_app]["comment"]))
 
-                print("Successfully created desktop entry at {}".format(desktop_entry_path))
+                print(
+                    "Successfully created desktop entry at {}".format(
+                        desktop_entry_path
+                    )
+                )
             except Exception as e:
                 print(e)
 
             # Create symlink
             try:
-                symlink_path = os.path.join('/usr/local/bin', selected_app)
+                symlink_path = os.path.join("/usr/local/bin", selected_app)
 
                 if os.path.exists(symlink_path):
                     print("Symlink already exists. Deleting it.")
                     os.remove(symlink_path)
 
-                os.symlink(os.path.join(install_path, 'bin', APP_LIST[selected_app]['executable'] + '.sh'), symlink_path)
+                os.symlink(
+                    os.path.join(
+                        install_path,
+                        "bin",
+                        APP_LIST[selected_app]["executable"] + ".sh",
+                    ),
+                    symlink_path,
+                )
 
                 print("Successfully created symlink at {}".format(symlink_path))
             except Exception as e:
@@ -430,16 +521,24 @@ class JetbrainsManagerTool:
 
             # Chmod +x on executable
             try:
-                executable_path = os.path.join(install_path, 'bin', APP_LIST[selected_app]['executable'] + '.sh')
-                subprocess.call(['sudo', 'chmod', '+x', executable_path])
-                print("Successfully set executable permissions on {}".format(executable_path))
+                executable_path = os.path.join(
+                    install_path, "bin", APP_LIST[selected_app]["executable"] + ".sh"
+                )
+                subprocess.call(["sudo", "chmod", "+x", executable_path])
+                print(
+                    "Successfully set executable permissions on {}".format(
+                        executable_path
+                    )
+                )
             except Exception as e:
                 print(e)
 
             # Remove downloaded file
             try:
                 os.remove(download_path)
-                print("Successfully removed downloaded file at {}".format(download_path))
+                print(
+                    "Successfully removed downloaded file at {}".format(download_path)
+                )
             except Exception as e:
                 print(e)
 
@@ -448,7 +547,7 @@ class JetbrainsManagerTool:
         for _ in range(max_redirects):
             response = requests.head(url, allow_redirects=False)
             if response.status_code in (301, 302):
-                url = response.headers.get('Location')
+                url = response.headers.get("Location")
             else:
                 return response.status_code
         return None
